5c8fb805387843404f89ca6ecfa6fb004be10752
makicamel <unright@gmail.com>
2019-04-01 23:34:58 +0900

    Study about adding constraint for generics.

--- generics.js
+++ generics.js
@@ -3,3 +3,15 @@ function getArray(value) {
 }
 console.log(getArray(10));
 console.log(getArray('hello'));
+var MyData = /** @class */ (function () {
+    function MyData(value) {
+        this.value = value;
+    }
+    ;
+    MyData.prototype.getArray = function () {
+        return [this.value, this.value, this.value];
+    };
+    return MyData;
+}());
+var myData = new MyData({ a: 100, b: 45 });
+console.log(myData.getArray());
--- generics.ts
+++ generics.ts
@@ -4,3 +4,17 @@ function getArray<T>(value: T): T[] {
 
 console.log(getArray<number>(10));
 console.log(getArray<string>('hello'));
+
+interface Result {
+  a: number;
+  b: number;
+}
+class MyData<T extends Result> {
+  constructor(public value: T) { };
+  getArray(): T[] {
+    return [this.value, this.value, this.value];
+  }
+}
+
+var myData = new MyData<Result>({ a: 100, b: 45 });
+console.log(myData.getArray());
