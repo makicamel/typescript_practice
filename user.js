776c7025bcbf475e7962defd4ea34710b0a62eeb
makicamel <unright@gmail.com>
2019-04-01 22:53:19 +0900

    Study about inherit class.

--- user.js
+++ user.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var User = /** @class */ (function () {
     function User(_name) {
         this._name = _name;
@@ -17,3 +30,19 @@ var User = /** @class */ (function () {
     });
     return User;
 }());
+var AdminUser = /** @class */ (function (_super) {
+    __extends(AdminUser, _super);
+    function AdminUser(_name, _age) {
+        var _this = _super.call(this, _name) || this;
+        _this._age = _age;
+        return _this;
+    }
+    AdminUser.prototype.sayHi = function () {
+        console.log("my age: " + this._age);
+        console.log("my name: " + this._name);
+        _super.prototype.sayHi.call(this);
+    };
+    return AdminUser;
+}(User));
+var user = new AdminUser('Maki', 34);
+user.sayHi();
--- user.ts
+++ user.ts
@@ -1,5 +1,5 @@
 class User {
-  constructor(private _name: string) {
+  constructor(protected _name: string) {
   }
   sayHi(): void {
     console.log(`Hi! I am ${this._name}.`);
@@ -11,3 +11,19 @@ class User {
     this._name = newValue;
   }
 }
+
+class AdminUser extends User {
+  private _age: number;
+  constructor(_name: string, _age: number) {
+    super(_name);
+    this._age = _age;
+  }
+  sayHi(): void {
+    console.log(`my age: ${this._age}`);
+    console.log(`my name: ${this._name}`);
+    super.sayHi();
+  }
+}
+
+var user = new AdminUser('Maki', 34);
+user.sayHi();
