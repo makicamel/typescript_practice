7030564942207d8bf31a9f5fa0e5c7634c4b672f
makicamel <unright@gmail.com>
2019-04-02 23:52:40 +0900

    [WIP]Study about outerModule; CommonJS and RequireJS.

--- module.js
+++ module.js
@@ -1,12 +1,13 @@
-var UserModule;
-(function (UserModule) {
-    UserModule.name = 'Maki';
-    var AddressModule;
-    (function (AddressModule) {
-        AddressModule.zip = '111-1111';
-    })(AddressModule = UserModule.AddressModule || (UserModule.AddressModule = {}));
-})(UserModule || (UserModule = {}));
-console.log(UserModule.name);
-console.log(UserModule.AddressModule.zip);
-var addr = UserModule.AddressModule;
-console.log(addr.zip);
+define(["require", "exports", "./user_amd"], function (require, exports, user) {
+    "use strict";
+    exports.__esModule = true;
+    // tsc module.ts -m commonjs
+    console.log(user.name);
+});
+// -- inner module
+// <reference path="./userModule.ts" />
+// tsc module.ts --out all.js
+// console.log(UserModule.name);
+// console.log(UserModule.AddressModule.zip);
+// import addr = UserModule.AddressModule;
+// console.log(addr.zip)
--- module.ts
+++ module.ts
@@ -1,7 +1,15 @@
-/// <reference path="./userModule.ts" />
+// -- outer module
+// Node - CommonJS
+// RequireJS - AMD
+// import User = require('./user_commonjs')
+import user = require('./user_amd')
+// tsc module.ts -m commonjs
+console.log(user.name)
+// -- inner module
+// <reference path="./userModule.ts" />
 // tsc module.ts --out all.js
-console.log(UserModule.name);
-console.log(UserModule.AddressModule.zip);
+// console.log(UserModule.name);
+// console.log(UserModule.AddressModule.zip);
 
-import addr = UserModule.AddressModule;
-console.log(addr.zip);
+// import addr = UserModule.AddressModule;
+// console.log(addr.zip)
